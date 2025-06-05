55754af04281bb506f7b76acef7f380c3f62f200
lguzm77 <lguzmm77@icloud.com>
2025-06-05 08:21:29 -0400

    Fill out deployment script

--- cert-manager/deploy.sh
+++ cert-manager/deploy.sh
@@ -2,8 +2,15 @@
 
 
 deploy () {
-  # TODO: fill out cluster-issuer.yaml with variables
+  prinf "Deploying cert-manager"
+  local CLOUDFLARE_EMAIL
+  local CLOUDFLARE_API_TOKEN
+  read -r -p "Enter your cloudflare account email: "  CLOUDFLARE_EMAIL
+  read -r -p "Enter your cloudflare API token: " CLOUDFLARE_API_TOKEN
 
+  sed "s/{ { CLOUDFLARE_EMAIL } }/$CLOUDFLARE_EMAIL/g" cluster-issuer-template.yaml |
+    sed "s/{ { CLOUDFLARE_API_TOKEN } }/$CLOUDFLARE_API_TOKEN/g" > cluster-issuer.yaml
+  # Before running this command, make sure cluster-issuer.yaml exists 
   kubectl apply -f cluster-issuer.yaml
 }
 
