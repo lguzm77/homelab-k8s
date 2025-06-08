8d39a8c2fb253690c748a9ebd11f84aea5a6f501
lguzm77 <lguzmm77@icloud.com>
2025-06-08 09:34:35 -0400

    Add render command

--- cert-manager/deploy.sh
+++ cert-manager/deploy.sh
@@ -3,15 +3,7 @@
 
 deploy () {
   prinf "Deploying cert-manager"
-  local CLOUDFLARE_EMAIL
-  local CLOUDFLARE_API_TOKEN
-  read -r -p "Enter your cloudflare account email: "  CLOUDFLARE_EMAIL
-  read -r -p "Enter your cloudflare API token: " CLOUDFLARE_API_TOKEN
-
-  sed "s/{ { CLOUDFLARE_EMAIL } }/$CLOUDFLARE_EMAIL/g" cluster-issuer-template.yaml |
-    sed "s/{ { CLOUDFLARE_API_TOKEN } }/$CLOUDFLARE_API_TOKEN/g" > cluster-issuer.yaml
-  # Before running this command, make sure cluster-issuer.yaml exists 
-  kubectl apply -f cluster-issuer.yaml
+   kubectl apply -f cluster-issuer.yaml
 }
 
 install () {
@@ -19,9 +11,23 @@ install () {
   kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.17.2/cert-manager.yaml
 }
 
+render () {
+  local CLOUDFLARE_EMAIL
+  local CLOUDFLARE_API_TOKEN
+  echo "Rendering template files"
+
+  read -r -p "Enter your cloudflare account email: "  CLOUDFLARE_EMAIL
+  read -r -p "Enter your cloudflare API token: " CLOUDFLARE_API_TOKEN
+
+  sed "s/{ { CLOUDFLARE_EMAIL } }/$CLOUDFLARE_EMAIL/g" cluster-issuer-template.yaml |
+    sed "s/{ { CLOUDFLARE_API_TOKEN } }/$CLOUDFLARE_API_TOKEN/g" > cluster-issuer.yaml
+}
 
 
 case "$1" in
+  render)
+    render
+    ;;
   install)
     install
     ;;
