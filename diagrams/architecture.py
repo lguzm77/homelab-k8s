64dfafada971d4afb75f3f4355d7aa74f8995b77
lguzm77 <lguzmm77@icloud.com>
2025-06-13 05:26:31 -0400

    Update diagrams

--- diagrams/architecture.py
+++ diagrams/architecture.py
@@ -1,24 +1,26 @@
 from diagrams import Cluster, Diagram
 from diagrams.k8s.compute import Pod, StatefulSet
-from diagrams.k8s.network import Service
+from diagrams.k8s.network import Service, Ingress 
 from diagrams.k8s.storage import PV, PVC, StorageClass
 from diagrams.k8s.group import Namespace
 
 with Diagram("Pihole On Kubernetes Architecture", show=True):
     with Cluster("Namespace Pihole"):
         sts = StatefulSet("Pihole")
-        headless = Service("Headless")
 
+        headless = Service("Headless")
         web_svc = Service("Web Service")
         dns_service_tcp = Service("DNS Service TCP")
         dns_service_udp = Service("DNS Service UDP")
 
-        sts << web_svc
-        sts << dns_service_tcp
-        sts << dns_service_udp
-        
+        services = [headless,web_svc, dns_service_tcp, dns_service_udp]
 
+        for service in sevices:
+            sts << service
         sts << headless
+
+        ingresses = []
+
 
 
 
